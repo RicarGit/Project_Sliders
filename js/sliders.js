815e4455cb0a96eaa77ef067e6281dcfc2f8df01
Ricardo Gava <Ricardoagava@gmail.com>
2021-04-26 23:39:16 -0300

    slider functional, my code is a little mess but i do this on my own!!!

---
+++ js/sliders.js
@@ -0,0 +1,27 @@
+const images = document.querySelectorAll("img")
+const circleContainer = document.querySelector(".image-circles-container")
+
+images.forEach(() => {
+  const div = document.createElement('div')
+  circleContainer.appendChild(div).classList.add('image-circle')
+})
+
+const imageCircles = document.querySelectorAll('.image-circle')
+imageCircles[0].classList.add('carousel-active')
+
+imageCircles.forEach((circle, index) => circle.setAttribute('id', `${index}`))
+
+let slideIndex = 0
+
+imageCircles.forEach(circle => {
+
+  circle.addEventListener('click', (event) => {
+
+    images.forEach(image => image.classList.remove('show-img'))
+    imageCircles.forEach(circle => circle.classList.remove('carousel-active'))
+
+    slideIndex = event.target.getAttribute('id')
+    images[slideIndex].classList.add('show-img')
+    circle.classList.add('carousel-active')
+  })
+})
