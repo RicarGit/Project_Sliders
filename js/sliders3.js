a22fd9633b70d633c274f9c1c58af6911869a491
Ricardo Gava <Ricardoagava@gmail.com>
2021-04-29 21:10:45 -0300

    third slider done,use next and prev buttons to slide between the images, there's a counter at top left to see how many images and the current image position you are

--- js/sliders3.js
+++ js/sliders3.js
@@ -1,33 +1,43 @@
 const images3 = document.querySelectorAll(".slide3")
+const imageCounter = document.querySelector('.image-counter')
+const nextButton = document.querySelector('.next-button')
+const previousButton = document.querySelector('.prev-button')
 
 let slideIndex3 = 0
 
-let automaticSlider = () => {
-  slideIndex2++
+const currentImageCounter = () =>
+  imageCounter.textContent = `${slideIndex3 + 1}/${images3.length}`
+
+const removeClassFromImages = () =>
+  images3.forEach(image => image.classList.remove('show-img'))
+
+const addClassToImages = () =>
+  images3[slideIndex3].classList.add('show-img')
+
+const nextImage = () => {
+  slideIndex3++
 
   if (slideIndex3 > images3.length - 1) {
     slideIndex3 = 0
   }
-  images2.forEach(image => image.classList.remove('show-img'))
-  imageCircles2.forEach(circle => circle.classList.remove('carousel-active'))
 
-  imageCircles2[slideIndex2].classList.add('carousel-active')
-  images2[slideIndex2].classList.add('show-img')
+  removeClassFromImages()
+  addClassToImages()
+  currentImageCounter()
 }
 
-let slideTimer = setInterval(automaticSlider, 2000);
+const prevImage = () => {
+  slideIndex3--
 
-imageCircles2.forEach(circle => {
-  circle.addEventListener('click', (event) => {
-    clearInterval(slideTimer)
-    slideTimer = setInterval(automaticSlider, 2000);
+  if (slideIndex3 < 0) {
+    slideIndex3 = images3.length - 1
+  }
 
-    images2.forEach(image => image.classList.remove('show-img'))
-    imageCircles2.forEach(circle => circle.classList.remove('carousel-active'))
+  removeClassFromImages()
+  addClassToImages()
+  currentImageCounter()
+}
 
-    slideIndex2 = event.target.getAttribute('id')
-    images2[slideIndex2].classList.add('show-img')
-    circle.classList.add('carousel-active')
-  })
-
-})
+currentImageCounter()
+nextButton.addEventListener('click', nextImage)
+previousButton.addEventListener('click', prevImage)
