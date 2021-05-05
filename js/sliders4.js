79201fff6b8f6141c542f8d601e98edaefa0c96d
Ricardo Gava <Ricardoagava@gmail.com>
2021-05-05 02:33:36 -0300

    last slider using scroll to slide between the images

--- js/sliders4.js
+++ js/sliders4.js
@@ -1,53 +1,13 @@
-const images4 = document.querySelectorAll(".slide4")
-const circleContainer4 = document.querySelector(".image-circles-container4")
-const imageCounter4 = document.querySelector('.image-counter2')
+const slideImages = document.querySelector('.carousel-images.carousel4')
 
+slideImages.addEventListener('wheel', event => {
+  event.preventDefault()
 
-images4.forEach(() => {
-  const div = document.createElement('div')
-  circleContainer4.appendChild(div).classList.add('image-circle4')
-})
+  targetWidth = event.target.clientWidth
 
-const imageCircles4 = document.querySelectorAll('.image-circle4')
-imageCircles4[0].classList.add('carousel-active')
-
-imageCircles4.forEach((circle, index) => circle.setAttribute('id', `${index}`))
-
-let slideIndex4 = 0
-
-const currentImageCounter4 = () =>
-  imageCounter4.textContent = `${Number(slideIndex4) + 1}/${images4.length}`
-
-let automaticSlider4 = () => {
-  slideIndex4++
-
-  if (slideIndex4 > images4.length - 1) {
-    slideIndex4 = 0
+  if (event.deltaY > 0) {
+    event.target.scrollBy(targetWidth, 0)
+  } else {
+    event.target.scrollBy(-targetWidth, 0)
   }
-  images4.forEach(image => image.classList.remove('show-img'))
-  imageCircles4.forEach(circle => circle.classList.remove('carousel-active'))
-
-  imageCircles4[slideIndex4].classList.add('carousel-active')
-  images4[slideIndex4].classList.add('show-img')
-
-  currentImageCounter4()
-}
-
-let slideTimer4 = setInterval(automaticSlider4, 2000)
-
-imageCircles4.forEach(circle => {
-  circle.addEventListener('click', (event) => {
-    clearInterval(slideTimer4)
-
-    images4.forEach(image => image.classList.remove('show-img'))
-    imageCircles4.forEach(circle => circle.classList.remove('carousel-active'))
-
-    slideIndex4 = event.target.getAttribute('id')
-    images4[slideIndex4].classList.add('show-img')
-    circle.classList.add('carousel-active')
-
-    slideTimer4 = setInterval(automaticSlider4, 2000)
-    currentImageCounter4()
-  })
 })
-currentImageCounter4()
